02a0842195bfca7dac341ad39d1c248d1912f617
Tim McNamara <code@timmcnamara.co.nz>
2023-09-19 02:59:13 +1200

    Update starting point

--- data-processing/src/main.rs
+++ data-processing/src/main.rs
@@ -1,14 +1,32 @@
-trait DataPipeline {
+trait DataPipeline<T> {
     fn insert(&mut self, item: T);
-    
-	fn insert_many(&mut self, items: impl Iterator<Item = T>);
-    
-	fn num_valid(&self) -> u32;
-    
-	fn filter(&mut self, predicate: fn(&T) -> bool);
+
+    fn insert_many(&mut self, items: impl Iterator<Item = T>);
+
+    fn num_valid(&self) -> u32;
+
+    fn filter(&mut self, predicate: fn(&T) -> bool);
 }
 
 struct Validator<T>;
+
+impl<T> DataPipeline<T> for Validator<T> {
+    fn insert(&mut self, item: T) {
+        todo!()
+    }
+
+    fn insert_many(&mut self, items: impl Iterator<Item = T>) {
+        todo!()
+    }
+
+    fn num_valid(&self) -> u32 {
+        todo!()
+    }
+
+    fn filter(&mut self, predicate: fn(&T) -> bool) {
+        todo!()
+    }
+}
 
 fn main() {
     let mut vals = Validator::<i32> {};

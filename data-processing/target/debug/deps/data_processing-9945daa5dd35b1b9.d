7861e4d17197ead58df858ec97e65a0cd868f00f
Tim McNamara <code@timmcnamara.co.nz>
2023-09-19 02:56:21 +1200

    fix: Remove target directories from version control

--- data-processing/target/debug/deps/data_processing-9945daa5dd35b1b9.d
+++
@@ -1,8 +0,0 @@
-/home/tim/LinkedIn Learning/rust-for-java-developers-2484214/data-processing/target/debug/deps/data_processing-9945daa5dd35b1b9.rmeta: src/main.rs Cargo.toml
-
-/home/tim/LinkedIn Learning/rust-for-java-developers-2484214/data-processing/target/debug/deps/data_processing-9945daa5dd35b1b9.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- data-processing/target/debug/deps/data_processing-b48ec0c8de936cee.d
+++
@@ -1,8 +0,0 @@
-/home/tim/LinkedIn Learning/rust-for-java-developers-2484214/data-processing/target/debug/deps/data_processing-b48ec0c8de936cee.rmeta: src/main.rs Cargo.toml
-
-/home/tim/LinkedIn Learning/rust-for-java-developers-2484214/data-processing/target/debug/deps/data_processing-b48ec0c8de936cee.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
